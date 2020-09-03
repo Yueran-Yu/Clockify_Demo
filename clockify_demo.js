a49efad5f26563bfee1ed00da184301d3c273532
yueranyu <librayuyueran@gmail.com>
2020-09-03 12:42:22 -0500

    adjust footer, fix the bug, add button style

--- clockify_demo.js
+++ clockify_demo.js
@@ -1,23 +1,28 @@
+const content_page = document.querySelector(".content_page")
+const section_list = content_page.getElementsByTagName("section")
+// alert(section_list.length)
 
-
-  const content_page = document.querySelector(".content_page")
-  const section_list = content_page.getElementsByTagName("section")
-  // alert(section_list.length)
-
-  for (let i = 0; i < section_list.length; i++) {
-    // console.log(i%2)
-    if (i%2 !== 0) {
-      section_list[i].classList.add('even_section')
-    }
+for (let i = 0; i < section_list.length; i++) {
+  // console.log(i%2)
+  if (i % 2 !== 0) {
+    section_list[i].classList.add('even_section')
   }
+  else{
+    section_list[i].classList.add('odd_section')
+  }
+}
 
-function navButtonEffect(){
+function navButtonEffect() {
   const nav_button = document.querySelector('.nav_logo')
   const nav_bar = document.querySelector('.nav_items')
-  nav_button.addEventListener('click', ()=>{
+  nav_button.addEventListener('click', () => {
     console.log("worked")
     nav_bar.classList.toggle('nav_show')
   })
 }
+
+
+
+
 
 navButtonEffect();
