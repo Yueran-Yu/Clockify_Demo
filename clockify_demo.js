75fe08c4c5863ddcce9ea73ba16b72db52c0b2aa
yueranyu <librayuyueran@gmail.com>
2020-09-03 09:59:31 -0500

    nav bar toggle effect

--- clockify_demo.js
+++ clockify_demo.js
@@ -1,9 +1,8 @@
-window.addEventListener('load', function () {
+
 
   const content_page = document.querySelector(".content_page")
   const section_list = content_page.getElementsByTagName("section")
   // alert(section_list.length)
-
 
   for (let i = 0; i < section_list.length; i++) {
     // console.log(i%2)
@@ -11,4 +10,14 @@ window.addEventListener('load', function () {
       section_list[i].classList.add('even_section')
     }
   }
-})
+
+function navButtonEffect(){
+  const nav_button = document.querySelector('.nav_logo')
+  const nav_bar = document.querySelector('.nav_items')
+  nav_button.addEventListener('click', ()=>{
+    console.log("worked")
+    nav_bar.classList.toggle('nav_show')
+  })
+}
+
+navButtonEffect();
